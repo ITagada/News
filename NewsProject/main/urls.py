908ce06144ee5dc157e19a8cc244c2ad5a708e60
ITagada <139345922+ITagada@users.noreply.github.com>
2023-12-21 00:10:25 +0300

    com5
    Signed-off-by: ITagada <139345922+ITagada@users.noreply.github.com>

--- NewsProject/main/urls.py
+++ NewsProject/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import index, get_profession
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', index, name='home'),
+    path('Profession/<int:profession_id>', get_profession, name='profession')
 ]
--- NewsProject/main/views.py
+++ NewsProject/main/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
-from .models import Human
+from .models import Human, Profession
 
 
 def index(request):
     human = Human.objects.all()
-    return render(request, 'NewsProject/Humans.html', {'human': human, 'title': 'Люди'})
+    professions = Profession.objects.all()
+    context = {
+        'human': human,
+        'title': 'Люди',
+        'professions': professions
+    }
+    return render(request, 'NewsProject/Humans.html', context=context)
+
+
+def get_profession(request, profession_id):
+    human = Human.objects.filter(profession_id=profession_id)
+    professions = Profession.objects.all()
+    profession = Profession.objects.get(pk=profession_id)
+    context = {
+        'human': human,
+        'professions': professions,
+        'profession': profession
+    }
+    return render(request, 'NewsProject/Profession.html', context=context)
